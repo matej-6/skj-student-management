e1c879f9a6a06de2f4d033cff5f56b1aa2a3231c
matej-6 <matejbarca2006@gmail.com>
2024-12-06 10:30:30 +0100

    search 50%

--- src/app/student/student.service.ts
+++ src/app/student/student.service.ts
@@ -32,41 +32,82 @@ const starterStudents: Student[] = [
   providedIn: 'root',
 })
 export class StudentService {
-  private _students = signal(new Map<string, Student>());
+  private _students = signal(new Array<Student>());
+  students = this._students.asReadonly();
 
-  students = computed(() => {
-    const studentsList = Array.from(this._students().values());
+  private selectedFilter = signal<Filter>('Meno a priezvisko');
+  private searchVal = signal('');
 
-    studentsList.sort((a: Student, b: Student) => {
-      if (this.getId(a) < this.getId(b)) {
-        return -1;
-      }
-      if (this.getId(a) > this.getId(b)) {
-        return 1;
-      }
-      return 0;
-    });
+  selectFilter(f: Filter) {
+    this.selectedFilter.set(f);
+  }
 
-    return studentsList;
+  updateSearchVal(newSearch: string) {
+    this.searchVal.set(newSearch);
+  }
+
+  filteredStudents = computed(() => {
+    switch (this.selectedFilter()) {
+      case 'Meno a priezvisko':
+        let filtered = this._students().filter((val) => {
+          const map = val.meno + val.priezvisko;
+          return (
+            map.includes(this.searchVal()) ||
+            val.meno.includes(this.searchVal()) ||
+            val.priezvisko.includes(this.searchVal())
+          );
+        });
+        return filtered;
+      case 'Meno':
+        filtered = this._students().filter((val) => {
+          return val.meno.includes(this.searchVal());
+        });
+        return filtered;
+      case 'Priezvisko':
+        filtered = this._students().filter((val) =>
+          val.priezvisko.includes(this.searchVal())
+        );
+        return filtered;
+
+      default:
+        this.sortStudents();
+        return this._students();
+    }
   });
+
+  private sortStudents() {
+    this._students.update((prev) => {
+      prev.sort((a: Student, b: Student) => {
+        if (this.getId(a) < this.getId(b)) {
+          return -1;
+        }
+        if (this.getId(a) > this.getId(b)) {
+          return 1;
+        }
+        return 0;
+      });
+
+      return prev;
+    });
+  }
 
   constructor() {
     const savedStudents = localStorage.getItem('students');
     if (!savedStudents) {
-      const newStudents = new Map<string, Student>();
+      const newStudents = new Array<Student>(starterStudents.length);
       for (let i = 0; i < starterStudents.length; i++) {
-        newStudents.set(this.getId(starterStudents[i]), starterStudents[i]);
+        newStudents[i] = starterStudents[i];
       }
       this._students.set(newStudents);
       return;
     }
     try {
-      const studentsMap: Map<string, Student> = JSON.parse(savedStudents);
+      const studentsMap: Array<Student> = JSON.parse(savedStudents);
       this._students.set(studentsMap);
     } catch {
-      const newStudents = new Map<string, Student>();
+      const newStudents = new Array<Student>(starterStudents.length);
       for (let i = 0; i < starterStudents.length; i++) {
-        newStudents.set(this.getId(starterStudents[i]), starterStudents[i]);
+        newStudents[i] = starterStudents[i];
       }
       this._students.set(newStudents);
     }
@@ -79,4 +120,21 @@ export class StudentService {
   private saveToLocalStorage() {
     localStorage.setItem('students', JSON.stringify([...this._students()]));
   }
+
+  addStudent(s: Student) {
+    if (!s.meno || !s.priezvisko) {
+      return;
+    }
+
+    this._students.update((prev) => {
+      prev.push(s);
+      return prev;
+    });
+
+    this.sortStudents();
+  }
+
+  filterByName() {}
 }
+
+type Filter = 'Meno a priezvisko' | 'Meno' | 'Priezvisko';
